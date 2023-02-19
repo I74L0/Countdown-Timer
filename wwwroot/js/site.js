2b9c383164c4e874ef87278542af2055b92705a7
I74L0 <i7alog4m3r1@gmail.com>
2023-02-19 11:59:45 -0300

    Formatted today time

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.getElementById('start').addEventListener("click", function () {
 
-// Write your JavaScript code.
+    const date = new Date();
+});
+
+window.setInterval(function () {
+    const date = new Date("2023-1-1");
+
+    // Formats and display the local time
+    document.querySelector(".today").innerHTML = `Today: ${(date.getHours().length < 2) ? "0" + date.getHours() : date.getHours()}:${(date.getMinutes().length < 2) ? "0" + date.getMinutes() : date.getMinutes()}:${(date.getSeconds().length < 2) ? "0" + date.getSeconds() : date.getSeconds()} ${(date.getDate().length < 2) ? "0" + date.getDate() : date.getDate()}/${((date.getMonth() + 1).length < 2) ? (date.getMonth() + 1) : "0" + (date.getMonth() + 1)}/${(date.getFullYear().length < 2) ? "0" + date.getFullYear() : date.getFullYear()}`;
+}, 1000);
