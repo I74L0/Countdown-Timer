9a8016f231c1aa314f4ec4356c15b4c4ba06cbb6
I74L0 <i7alog4m3r1@gmail.com>
2023-02-20 10:43:40 -0300

    Finished funcionality

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,6 @@
 ﻿var event_date = "";
+
+// When button is clicked
 document.getElementById('start').addEventListener("click", function () {
     if (document.getElementById("name").value != '' && document.getElementById('date').value != '') {
         document.querySelector(".name").innerHTML = document.getElementById("name").value;
@@ -8,6 +10,9 @@ document.getElementById('start').addEventListener("click", function () {
         }
         event_date = new Date(`${document.getElementById('date').value} ${time}`);
         document.querySelector(".event").innerHTML = `Event date: ${(event_date.getHours().toString().length < 2) ? "0" + event_date.getHours() : event_date.getHours()}:${(event_date.getMinutes().toString().length < 2) ? "0" + event_date.getMinutes() : event_date.getMinutes()}:${(event_date.getSeconds().toString().length < 2) ? "0" + event_date.getSeconds() : event_date.getSeconds()} ${(event_date.getDate().toString().length < 2) ? "0" + event_date.getDate() : event_date.getDate()}/${((event_date.getMonth() + 1).toString().length < 2) ? "0" + (event_date.getMonth() + 1) : event_date.getMonth() + 1}/${event_date.getFullYear()}`;
+    }
+    else {
+        alert("Name or date can't be empty!");
     }
 });
 
@@ -25,6 +30,12 @@ window.setInterval(function () {
             document.querySelector(".timer #hours").innerHTML = Math.trunc(time_left % 86400 / 3600);
             document.querySelector(".timer #minutes").innerHTML = Math.trunc(time_left % 86400 % 3600 / 60);
             document.querySelector(".timer #seconds").innerHTML = Math.trunc(time_left % 86400 % 3600 % 60);
+        }
+        else {
+            document.querySelector(".timer #days").innerHTML = "00";
+            document.querySelector(".timer #hours").innerHTML = "00";
+            document.querySelector(".timer #minutes").innerHTML = "00";
+            document.querySelector(".timer #seconds").innerHTML = "00";
         }
     }
 }, 1000);
